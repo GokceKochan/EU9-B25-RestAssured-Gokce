458c3fbac46ff0e6e2e3719b35f2a74aa847c25e
gokceeksioglu <gokceeksioglu@hotmail.com>
2022-11-19 22:50:01 +0000

    Simple get request

--- src/main/java/cydeo/Day1/simpleGetRequest.java
+++ src/main/java/cydeo/Day1/simpleGetRequest.java
@@ -1,8 +1,29 @@
 package cydeo.Day1;
 
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+
 public class simpleGetRequest {
 
-    
+    String url = "http://54.208.45.78:8000/api/spartans";
+
+
+    @Test
+    public void test1(){
+
+        //send a get request and save response inside the Response object
+        Response response= RestAssured.get(url);
+
+        //print Response status code
+        System.out.println(response.statusCode());
+
+        //print response body
+        response.prettyPrint();
+
+
+    }
 
 
 
